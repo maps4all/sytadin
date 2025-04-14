e518fa640330db88b2c52bef96dc32c19b5a7c42
maps4all <maps4all007@yandex.com>
2025-04-14 12:18:29 +0200

    https for all.

--- plugin.js
+++ plugin.js
@@ -9,12 +9,12 @@
     "screenLockRot" : false,
     "reload" : true,
     
-    "shouldOverrideUrlLoading1" : "http://m.sytadin.fr",
+    "shouldOverrideUrlLoading1" : "https://m.sytadin.fr",
     "shouldOverrideUrlLoading2" : "https://m.sytadin.fr",
 
-    "loadUrl" : "http://m.sytadin.fr",
+    "loadUrl" : "https://m.sytadin.fr",
     			
-    "browserLaunchLink" : "http://m.sytadin.fr",
+    "browserLaunchLink" : "https://m.sytadin.fr",
     
     "onPageFinishedLoadUrl" : "javascript: $('#splashScreen').hide(); var newLonLat = new OpenLayers.LonLat(#lng#, #lat#).transform( new OpenLayers.Projection('EPSG:4326'), map.getProjectionObject() );map.setCenter(new OpenLayers.LonLat( newLonLat.lon, newLonLat.lat ), #zoom# - 10, false, true);"
 
